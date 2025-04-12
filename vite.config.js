2083c5a4dee31e01c858de7b6beca8bcd79bf314
www-data <www-data@MichaelsRogue>
2025-04-12 19:30:12 +0200

    added base path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     root: 'src',
+    base: '/twothreebird-tic-tac-toe/',
     build: {
         outDir: '../dist',
         emptyOutDir: true
